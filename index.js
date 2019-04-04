4120344852fdf782f54930d1170458a02504feda
smiling-analyst-8016 <erickdalcantara@gmail.com>
2019-04-04 14:26:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,10 +16,21 @@ function takeANumber(katzDeli, name) {
  }
 
 
- function currentLine(katzDeli) {
+// Build a function `currentLine` that accepts the current line of people and returns the current line as a string; 
+// for example, if 'katzDeliLine' is currently `["Ada", "Grace"]`, 
+// `currentLine(katzDeliLine)` would return `"The line is currently: 1. Ada, 2. Grace"`.  
+// You don't have to use `katzDeliLine` as a variable or parameter name in your function though, 
+// it's just an example of a variable that might be passed to it. 
+// If there is nobody in line, it should return `"The line is currently empty."`
 
-   for (let i = 0; i < katzDeli.length; i++) {
-      line.push(` `+[i+1]+`. `  + katzDeli[i])
-    }
-    return(`The line is currently:` + line);
+function currentLine(katzDeli) {
+  let i = 0
+  while (i < katzDeli.length) {
+    line.push(` `+[i+1]+`. `  + katzDeli[i])
+    i++
   }
+  if (katzDeli.length === 0) {
+    return "The line is currently empty."
+  } else
+  return(`The line is currently:` + line)
+}
