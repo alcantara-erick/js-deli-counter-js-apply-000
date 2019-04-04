3a7c33b9697f0ef90a7b5afcbeb9b8d4e1901809
smiling-analyst-8016 <erickdalcantara@gmail.com>
2019-04-04 14:15:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function takeANumber(katzDeli, name) {
   katzDeli.push(`${name}`)
-   return("Welcome, ${name}. You are number ${katzDeli.length} in line."")
+   return("Welcome, ${name}. You are number ${katzDeli.length} in line.")
  }
- 
